314b89176b1f6d88c00dba82a25ae1f20f7d2d61
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2021-03-14 00:37:32 +0100

    Change 2/ example filenames to be grouped by slide

--- 2.memory-allocation/4.1.c
+++ 2.memory-allocation/4.1.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
-char buffer[124];
+char a;
+int b;
+char c;
+char d;
+char e;
 
 int main(int argc, char const *argv[])
 {
-    printf("Buffer address: %p\n", buffer);
+    printf("&d: %p\n", &d);
+    printf("&b: %p\n", &b);
+    printf("&e: %p\n", &e);
+    printf("&c: %p\n", &c);
+    printf("&a: %p\n", &a);
     return 0;
 }
