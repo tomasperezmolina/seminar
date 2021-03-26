537590839db5183b5f8056fe0ed4b80f07230b70
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2021-03-26 17:55:57 +0100

    Update 10.2

--- 2.memory-allocation/10.2.c
+++ 2.memory-allocation/10.2.c
@@ -3,14 +3,14 @@
 void print_hello_static() {
     char *str = "hello static!";
     printf("%s\n", str);
-    str[0]++;
+    str[0] = 'i';
     printf("%s\n", str);
 }
 
 void print_hello_stack() {
     char str[] = "hello stack!";
     printf("%s\n", str);
-    str[0]++;
+    str[0] = 'i';
     printf("%s\n", str);
 }
 
